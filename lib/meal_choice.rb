580ddab6d79c29c3218b9eabe6f862ea4c8e3ac6
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-09 17:19:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,3 @@
-# Your code here
-"A plate of #{protein} with #{veg1} and #{veg2}."
+def meal_choice (veg1, veg2, protein = "meat")# Your code here
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+end
